829efcc99d14431c69d769d711aa894bbcb8e4d7
cainemerrick98 <104197169+cainemerrick98@users.noreply.github.com>
2026-01-03 16:17:32 +0000

    formatting

--- src/tools.py
+++ src/tools.py
@@ -6,11 +6,9 @@ from resolver import resolve_query
 from compiler import compile_sql
 
 def make_get_data(data_connection: BaseDataConnection):
-    
     def get_data(query: Query) -> dict:
         resolved = resolve_query(query)
         sql = compile_sql(resolved)
         data = data_connection.query(sql)
         return data
-
     return get_data
